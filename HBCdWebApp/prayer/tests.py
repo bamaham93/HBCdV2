45d002a5966210a0abd8798374625f9461641f1d
Jacob McGowin <bamaham93@gmail.com>
2024-09-26 02:23:45 -0400

    Added a test for testing.

--- HBCdWebApp/prayer/tests.py
+++ HBCdWebApp/prayer/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from prayer.models import Request
 
 # Create your tests here.
+class TestRequests(TestCase):
+    """
+    """
+
+    def test_nothing(self):
+        self.assertEqual(True, True)
