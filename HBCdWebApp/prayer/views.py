502abf13ec62a11d3568475e69df6153ae059c29
Jacob McGowin <bamaham93@gmail.com>
2024-09-26 03:51:32 -0400

    Added table to home page to display requests.

--- HBCdWebApp/prayer/views.py
+++ HBCdWebApp/prayer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from prayer.forms import RequestForm
+from prayer.models import Request
 
 
 # Create your views here.
@@ -9,12 +10,16 @@ def home(request):
     :return:
     """
     request_form = RequestForm()
-    context = {'form': request_form}
+    context = {
+        'form': request_form,
+    }
 
     if request.method == 'POST':
         print('I got a postcard!')
 
     if request.user.is_authenticated:
         print('I know me!')
+
+        context['requests'] = Request.objects.all()
 
     return render(request, 'prayer/home.html', context)
