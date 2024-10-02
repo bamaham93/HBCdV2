48ba37a4a71ed56f16c1be289d4254d08e08d4fd
Jacob McGowin <bamaham93@gmail.com>
2024-10-02 16:07:49 -0400

    Added static_root to settings.py.

--- HBCdWebApp/HBCdWebApp/settings.py
+++ HBCdWebApp/HBCdWebApp/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/home/Bamaham93/hbcv2/HBCdV2/HBCdWebApp/static"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     # "/var/www/static/",
