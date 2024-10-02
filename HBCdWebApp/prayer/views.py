cb804b70d550dc1094dc3fbd4770678c1ab987c6
Jacob McGowin <bamaham93@gmail.com>
2024-10-02 12:33:15 -0400

    Added Prayer Group membership as requirement for data access.

--- HBCdWebApp/prayer/views.py
+++ HBCdWebApp/prayer/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from prayer.forms import RequestForm
 from prayer.models import Request
+from django.contrib.auth.models import User, Group
 
+
+def is_member_of(user, group_name: str, user_groups):
+    """
+    :param user The user making the request
+    :param user_groups The groups to which that user belongs.
+    :return: Bool
+
+    Checks to see if the user is a member of the group, e.g. group_name='Prayer Group'
+    """
+    # print(user.groups.filter(name=group_name).exists)
+    # if user.groups.filter(name=group_name.exists()):
+    #     return True
+    # else:
+    #     return False
+
+    if group_name in user_groups:
+        return True
+    else:
+        return False
 
 # Create your views here.
-
 def home(request):
     """
     :return:
@@ -22,6 +41,9 @@ def home(request):
             print(request.POST)
             form.save()
             print('Form is saved! Hallelujah!')
+
+    # print(is_member_of(user=request.user, group_name="Prayer Group", user_groups=request.user.groups.all()))
+    # print(request.user.groups.all())
 
     if request.user.is_authenticated:
         # print('I know me!')
